5e6c7b74ba982904e31c1333362a6f0499ffd938
Ana Clara <anaclaramelo2707@gmail.com>
2024-05-25 11:47:34 -0300

    adicionando mapa interativo, normalizando css e adicionando fonte museo

---
+++ js/carScroll.js
@@ -0,0 +1,23 @@
+let rotationAngle = 20; // Começa com 20 graus
+
+window.addEventListener('scroll', function() {
+    const scrollPosition = window.scrollY;
+    const fixedDiv = document.getElementById('MyImage');
+    
+    console.log('scroll: ' + scrollPosition);
+
+    // Verifica se o scroll aumentou ou diminuiu 20 unidades
+    if (scrollPosition % 20 === 0) {
+        // Alterna entre 20 e -20 graus
+        rotationAngle = rotationAngle === 20 ? -20 : 20;
+        fixedDiv.style.transform = `rotate(${rotationAngle}deg) translateX(${rotationAngle}px) `;
+    }
+});
+
+
+/*
+window.addEventListener('scroll', function() {
+    var img = document.getElementById("MyImage");
+    var rect = img.getBoundingClientRect();
+    console.log("Posição da imagem no scroll: " + rect.top);
+})*/
