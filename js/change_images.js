33b2832ade05e5c17f4e6967d8b7e28305c26fdc
Ana Clara Melo <132848065+anaclaraw@users.noreply.github.com>
2024-11-04 23:15:58 -0300

    atualização antes da apresentação do projeto - melhorando detalhes

--- js/change_images.js
+++ js/change_images.js
@@ -1,7 +1,7 @@
 var imagem_carrousel1 = document.getElementById('img_pista_back')
 
-if (window.innerWidth < 500) {
-    imagem_carrousel1.src = './assets/pista_background_mobile2.png'
+if (window.innerWidth < 767.98) {
+    imagem_carrousel1.src = './assets/pista_background_mobile_2.png'
 }else{
     imagem_carrousel1.src = './assets/pista_background2.png'
     }
